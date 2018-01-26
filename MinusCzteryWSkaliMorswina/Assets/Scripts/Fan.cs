f9a073e44f49e1e8ea75affa970c04723793e8b5
Krzysztof Skoracki <krzysztof@skoracki.net>
2018-01-27 00:58:45 +0100

    Add scripts

---
+++ MinusCzteryWSkaliMorswina/Assets/Scripts/Fan.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Rotate(new Vector3(0, 0, -100 * horizontal * Time.deltaTime));
+	}
+}
---
+++ MinusCzteryWSkaliMorswina/Assets/Scripts/Ship.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+    public GameObject fan;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Collider2D col = fan.GetComponent<Collider2D>();
+
+        bool inFan = col.OverlapPoint(new Vector2(transform.position.x, transform.position.y));
+        Debug.Log(inFan);
+
+        if (inFan)
+        {
+            Vector3 direction = fan.transform.rotation * new Vector3(0, 1, 1);
+            transform.position += Time.deltaTime * 1 * direction;
+        }
+	}
+}
