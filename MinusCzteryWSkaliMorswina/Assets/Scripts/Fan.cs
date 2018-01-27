d9b74c634007695a9411708c83298d9c420d7f63
Lukasz Jagodzinski <lukasz.jagodzinski@stxnext.pl>
2018-01-27 21:03:01 +0100

    add fan switch

--- MinusCzteryWSkaliMorswina/Assets/Scripts/Fan.cs
+++ MinusCzteryWSkaliMorswina/Assets/Scripts/Fan.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Fan : MonoBehaviour {
 
+	public bool isActive = false;
+
 	// Use this for initialization
 	void Start () {
-		
+		GameState gameState = GameObject.Find("GameState").GetComponent<GameState>();
+		gameState.fanlist.Add(this);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float horizontal = Input.GetAxis("Horizontal");
-        transform.Rotate(new Vector3(0, 0, -100 * horizontal * Time.deltaTime));
+		if (isActive) {
+			transform.Rotate (new Vector3 (0, 0, -100 * horizontal * Time.deltaTime));
+		}
 	}
 }
--- MinusCzteryWSkaliMorswina/Assets/Scripts/GameState.cs
+++ MinusCzteryWSkaliMorswina/Assets/Scripts/GameState.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class GameState : MonoBehaviour {
 
+	public List<Fan> fanlist = new List<Fan>();
+
     public GameObject CurrentFan;
+	public Fan currentFan;
 
 	// Use this for initialization
 	void Start () {
-		
+		currentFan = CurrentFan.GetComponent<Fan> ();
+		currentFan.isActive = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown ("tab")) {
+			int idx = (fanlist.IndexOf (currentFan)+1)%fanlist.Count;
+			currentFan.isActive = false;
+			currentFan = fanlist [idx];
+			currentFan.isActive = true;
+		}
 	}
 }
