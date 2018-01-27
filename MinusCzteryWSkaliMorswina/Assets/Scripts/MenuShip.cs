eb5990008e0aaf3d6cbe45bc6e6cdc031f4203c2
Lukasz Jagodzinski <lukasz.jagodzinski@stxnext.pl>
2018-01-27 23:43:41 +0100

    add menu

---
+++ MinusCzteryWSkaliMorswina/Assets/Scripts/MenuShip.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuShip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Update () {
+		Vector3 movement = new Vector3 (0, 1, 0);
+		transform.Translate(movement * Time.deltaTime * 0.2f);
+
+	}
+}
