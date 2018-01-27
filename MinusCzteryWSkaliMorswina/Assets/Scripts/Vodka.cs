4cad5370e1b8ab09cfdd428a9d2a6307fb8dc046
Krzysztof Skoracki <krzysztof@skoracki.net>
2018-01-27 21:15:17 +0100

    The water must flow

---
+++ MinusCzteryWSkaliMorswina/Assets/Scripts/Vodka.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vodka : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(Mathf.Sin(Time.time * 0.2f) * 0.005f, Mathf.Sin(Time.time * 0.2f) * 0.005f, 0);	
+	}
+}
